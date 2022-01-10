bb20104313d7baf50e7bcd81cf00da82c3c7d588
Mitali Garg <72097687+Mitali-13@users.noreply.github.com>
2022-01-10 21:36:44 +0530

    Code to send SMS

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace LearnTwilio //collection of classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            var accountSid=Secrets.AccountSid;
+            var authToken=Secrets.AuthToken;
+            var toNumber = Secrets.ToNumber;
+            var fromNumber = Secrets.FromNumber;
+
+            TwilioClient.Init(accountSid,authToken);
+
+            var message = MessageResource.Create(
+                to : new PhoneNumber(toNumber),
+                from : new PhoneNumber(fromNumber),
+                body: "Hi. Having fun at MLH LHD Build!"
+            );
+
+            Console.WriteLine(message.Sid);
+
+        }
+    }
+}
